67729295f42982f306e9b99cc26897f244425dd9
himelpvz <himelparvez39@gmail.com>
2025-11-27 11:13:55 +0600

    added gpt utils

--- device.mk
+++ device.mk
@@ -38,6 +38,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+    #added later I don't know
+    
+    
+PRODUCT_PACKAGES += vndservicemanager
+PRODUCT_PACKAGES += vndservice
+PRODUCT_PACKAGES += libhidltransport.vendor
 
 PRODUCT_PACKAGES += \
     otapreopt_script
