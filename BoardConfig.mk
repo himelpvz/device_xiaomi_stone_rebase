485bee6e47c053a4fa18bbdfe47d27531a3dbcf5
Himel Parvez <himelparvez39@gmail.com>
2025-11-28 00:18:30 +0600

    Comment out build flags in BoardConfig.mk
    Comment out build flags for better clarity.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,14 @@
 DEVICE_PATH := device/xiaomi/stone
 
 # ============================================
+# Build Flags
+# ============================================
+#BUILD_BROKEN_DUP_RULES := true
+#BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+#BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+#ALLOW_MISSING_DEPENDENCIES := true
+
+# ============================================
 # Architecture
 # ============================================
 
