263db26f7ed06a2b17a37b532a945929db63c33d
Himel Parvez <himelparvez39@gmail.com>
2025-11-30 01:25:38 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,9 +6,9 @@ DEVICE_PATH := device/xiaomi/stone
 # ============================================
 # Build Flags
 # ============================================
-#BUILD_BROKEN_DUP_RULES := true
-#BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-#BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 ALLOW_MISSING_DEPENDENCIES := true
 
 # ============================================
