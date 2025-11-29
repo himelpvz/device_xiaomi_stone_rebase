0e9e43cdc40aac85c5c3ae8f4f58f647c442c31c
Himel Parvez <himelparvez39@gmail.com>
2025-11-29 14:32:29 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,11 +6,12 @@
 # Add your device's build targets to the lunch menu.
 # The 'omni_' prefix is often used for generic TWRP builds.
 # The targets are usually 'eng' (engineering) and 'userdebug'.
+git clone https://github.com/bittencourtjulival/eclipse_kernel_xiaomi_stone -b 16 kernel/xiaomi/stone --depth=1
 
 # Syntax: add_lunch_combo <product_name>-<build_variant>
 add_lunch_combo twrp_stone-eng
-add_lunch_combo twrp_stone-user
-add_lunch_combo twrp_stone-userdebug
+#add_lunch_combo twrp_stone-user
+#add_lunch_combo twrp_stone-userdebug
 
 # Or, if using the older OmniROM naming convention:
 # add_lunch_combo omni_<device-codename>-eng
