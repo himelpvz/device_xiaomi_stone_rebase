6d92c7ee4c5e6d1e041629aca856c5bc743a67cf
Himel Parvez <himelparvez39@gmail.com>
2025-11-29 14:25:02 +0600

    Update BoardConfig.mk for vendor_boot.img
    Added 
    ## BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
    
    For vendor_boot header 4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ BOARD_VENDOR := xiaomi
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 
-BOARD_BOOT_HEADER_VERSION := 3
+BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
@@ -103,7 +103,7 @@ BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
 # Recovery - Vendor Boot (CRITICAL SECTION)
 # ============================================
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-
+BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
 # ============================================
 # A/B & Dynamic Partitions
 # ============================================
